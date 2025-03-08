b9a308c10d1b530df913725ea3b388f6902f5084
musab <musabjamil3@gmail.com>
2025-03-08 13:36:05 +0500

    Resolved conflicts

--- ApplicationDevelopmentProject.py
+++ ApplicationDevelopmentProject.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # ApplicationDevelopmentProject.py
 import dash
 from dash import html, dcc, Input, Output
@@ -76,83 +75,4 @@ def display_page(pathname):
         return html.Div([
             html.H2("Welcome to the Application Development Project"),
             html.P("Please select a page from the menu.")
-=======
-# ApplicationDevelopmentProject.py
-import dash
-from dash import html, dcc, Input, Output
-import share, trend, sentiment
-
-app = dash.Dash(__name__, suppress_callback_exceptions=True)
-
-app.layout = html.Div([
-    html.H1('Application Development Project'),
-    dcc.Location(id='url', refresh=False),
-    html.Div([
-        html.H1("Application Development Project", style={'textAlign': 'center', 'fontSize': '70px', 'marginBottom': '5px', 'color': '#FFF'}),
-        html.Hr(style={'width': '100%'}),
-    ]),
-    html.Div([
-        dcc.Link(
-            'Shares Analysis',
-            href='/share',
-            style={
-                'marginRight':'10px',
-                'padding': '10px',
-                'backgroundColor': '#3C3C3C',
-                'border': '1px solid #FFF',
-                'textDecoration': 'none',
-                'color': '#F3F3F3'
-            }
-        ),
-        dcc.Link(
-            'Trend Analysis',
-            href='/trend',
-            style={
-                'backgroundColor': '#3C3C3C',
-                'border': '1px solid #FFF',
-                'textDecoration': 'none',
-                'color': '#FFF',
-                'marginRight':'10px',
-                'padding': '10px',
-
-            }
-        ),
-        dcc.Link(
-            'Sentiment Analysis',
-            href='/sentiment',
-            style={
-                'marginRight':'10px',
-                'padding': '10px',
-                'backgroundColor': '#3C3C3C',
-                'border': '1px solid #FFF',
-                'textDecoration': 'none',
-                'color': '#FFF'
-            }
-        ),
-    ], style={
-        'display':'flex',
-        'justifyContent':'center',
-        'backgroundColor': '#1E1E1E',
-    }),
-    html.Hr(style={'width': '100%'}),
-    html.Div(id='page-content'),
-], style={
-    'backgroundColor': '#1E1E1E',
-    'color': '#FFF'  # White text
-})
-
-@app.callback(Output('page-content', 'children'),
-              Input('url', 'pathname'))
-def display_page(pathname):
-    if pathname == '/share':
-        return share.layout
-    elif pathname == '/trend':
-        return trend.layout
-    elif pathname == '/sentiment':
-        return sentiment.layout
-    else:
-        return html.Div([
-            html.H2("Welcome to the Application Development Project"),
-            html.P("Please select a page from the menu.")
->>>>>>> master
         ])
--- home.py
+++ home.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from dash import html
 
 layout= html.Div([
@@ -7,14 +6,4 @@ layout= html.Div([
         html.P("We use advanced data analysis techniques to provide insights into trends and sentiments in news articles published by Mashable.", style={'textAlign': 'center'}),
         html.P("Most recently we've added some ML (Machine Learning) work as well, where we've included the results of classifying fake and authentic news. This was done on a dataset from Kaggle.", style={'textAlign': 'center'}),
         html.P("Feel free to explore the website!", style={'textAlign': 'center'}),
-=======
-from dash import html
-
-layout= html.Div([
-        html.H1("Welcome to UnMashable!", style={'textAlign': 'center','fontsize': '30px'}),
-        html.P("UnMashable is a data analysis project focused on unraveling news articles, one byte at a time!", style={'textAlign': 'center'}),
-        html.P("We use advanced data analysis techniques to provide insights into trends and sentiments in news articles published by Mashable.", style={'textAlign': 'center'}),
-        html.P("Most recently we've added some ML (Machine Learning) work as well, where we've included the results of classifying fake and authentic news. This was done on a dataset from Kaggle.", style={'textAlign': 'center'}),
-        html.P("Feel free to explore the website!", style={'textAlign': 'center'}),
->>>>>>> master
     ])
